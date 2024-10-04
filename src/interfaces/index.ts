b617ed3a86a5e7e7be2f2f773b79c4e4ec99fdb0
AVRADEEP NAYAK <avradeepnayak@AVRADEEPs-MacBook-Air.local>
2024-10-04 18:10:00 +0530

    v0.0.1

---
+++ src/interfaces/index.ts
@@ -0,0 +1,23 @@
+export type IconName =
+  | 'arrow-up'
+  | 'chevron-down'
+  | 'chevron-left'
+  | 'chevron-right'
+  | 'cross'
+  | 'filters-2'
+  | 'filters-3'
+  | 'plus'
+  | 'search';
+
+export interface Event {
+  id: string;
+  title: string;
+  description?: string;
+  datetime: number;
+  date: number;
+  time: number;
+  location: string;
+  category: string;
+  picture?: string;
+  priority: string;
+}
